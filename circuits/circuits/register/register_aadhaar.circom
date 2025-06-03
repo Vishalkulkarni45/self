46f6b9108493f0dc51c5bea7b19f0f6d4190bcb8
vishal <vishalkoolkarni0045@gmail.com>
2025-06-03 13:12:18 +0530

    feat(wip): register circuit for aadhaar

---
+++ circuits/circuits/register/register_aadhaar.circom
@@ -0,0 +1,48 @@
+pragma circom 2.1.9;
+
+include "circomlib/circuits/bitify.circom";
+include "circomlib/circuits/poseidon.circom";
+//include "annon-aadhaar/packages/circuits/src/helpers/signature.circom";
+include "@anon-aadhaar/circuits/helpers/signature.circom";
+/// @title: AadhaarRegister
+/// @notice Main circuit — verifies the integrity of the aadhaar data, the signature, and generates commitment and nullifier
+/// @param n RSA pubic key size per chunk
+/// @param k Number of chunks the RSA public key is split into
+/// @param maxDataLength Maximum length of the data
+/// @input qrDataPadded QR data without the signature; assumes elements to be bytes; remaining space is padded with 0
+/// @input qrDataPaddedLength Length of padded QR data
+/// @input delimiterIndices Indices of delimiters (255) in the QR text data. 18 delimiters including photo
+/// @input signature RSA signature
+/// @input pubKey RSA public key (of the government)
+
+// TODO: Import SignatureVerifier from @annon-aadhaar/circuits
+
+template REGISTER_AADHAAR(n, k, maxDataLength){
+
+    signal input qrDataPadded[maxDataLength];
+    signal input qrDataPaddedLength;
+    signal input pubKey[k];
+    signal input signature[k];
+
+    //signal output pubkeyHash;
+
+  // Assert `qrDataPaddedLength` fits in `ceil(log2(maxDataLength))`
+  //TODO: how to use log2Ceil?
+  //  component n2bHeaderLength = Num2Bits(log2Ceil(maxDataLength));
+    component n2bHeaderLength = Num2Bits(11);
+    n2bHeaderLength.in <== qrDataPaddedLength;
+
+    // optimize: SigVerify internally hashes the pubkey if not needed we can remove it .
+    // Verify the RSA signature
+    // component signatureVerifier = SignatureVerifier(n, k, maxDataLength);
+    // signatureVerifier.qrDataPadded <== qrDataPadded;
+    // signatureVerifier.qrDataPaddedLength <== qrDataPaddedLength;
+    // signatureVerifier.pubKey <== pubKey;
+    // signatureVerifier.signature <== signature;
+    // pubkeyHash <== signatureVerifier.pubkeyHash;
+
+
+}
+
+
+component main = REGISTER_AADHAAR(121, 17, 512 * 3);
---
+++ circuits/tests/register/register_aadhaar.test.ts
@@ -0,0 +1,98 @@
+import { expect } from 'chai';
+import { wasm as wasmTester } from 'circom_tester';
+import path from 'path';
+
+import { sha256Pad } from '@zk-email/helpers/dist/sha-utils';
+import {
+  bigIntToChunkedBytes,
+  bufferToHex,
+  Uint8ArrayToCharArray,
+} from '@zk-email/helpers/dist/binary-format';
+import {
+  convertBigIntToByteArray,
+  decompressByteArray,
+  splitToWords,
+  extractPhoto,
+  timestampToUTCUnix,
+} from '@anon-aadhaar/core';
+
+import fs from 'fs';
+import crypto from 'crypto';
+import assert from 'assert';
+import { testQRData } from '../assets/dataInput.json';
+
+let QRData: string = testQRData;
+
+function prepareTestData() {
+  const qrDataBytes = convertBigIntToByteArray(BigInt(QRData));
+  const decodedData = decompressByteArray(qrDataBytes);
+
+  const signatureBytes = decodedData.slice(decodedData.length - 256, decodedData.length);
+
+  const signedData = decodedData.slice(0, decodedData.length - 256);
+
+  const [qrDataPadded, qrDataPaddedLen] = sha256Pad(signedData, 512 * 3);
+
+  const delimiterIndices: number[] = [];
+  for (let i = 0; i < qrDataPadded.length; i++) {
+    if (qrDataPadded[i] === 255) {
+      delimiterIndices.push(i);
+    }
+    if (delimiterIndices.length === 18) {
+      break;
+    }
+  }
+
+  const signature = BigInt('0x' + bufferToHex(Buffer.from(signatureBytes)).toString());
+
+  const pkPem = fs.readFileSync(path.join(__dirname, '../assets/testPublicKey.pem'));
+  const pk = crypto.createPublicKey(pkPem);
+
+  const pubKey = BigInt(
+    '0x' + bufferToHex(Buffer.from(pk.export({ format: 'jwk' }).n as string, 'base64url'))
+  );
+
+  const inputs = {
+    qrDataPadded: Uint8ArrayToCharArray(qrDataPadded),
+    qrDataPaddedLength: qrDataPaddedLen,
+    signature: splitToWords(signature, BigInt(121), BigInt(17)),
+    pubKey: splitToWords(pubKey, BigInt(121), BigInt(17)),
+  };
+
+  return {
+    inputs,
+    qrDataPadded,
+    signedData,
+    decodedData,
+    pubKey,
+    qrDataPaddedLen,
+  };
+}
+
+describe(' REGISTER AADHAAR Circuit Tests', () => {
+  let circuit;
+  before(async function () {
+    this.timeout(0);
+    circuit = await wasmTester(
+      path.join(__dirname, '../../circuits/register/register_aadhaar.circom'),
+      {
+        verbose: true,
+        logOutput: true,
+        include: [
+          'node_modules',
+          './node_modules',
+          './node_modules/circomlib/circuits',
+          //'./node_modules/@annon-aadhaar',
+        ],
+      }
+    );
+  });
+
+  it('should compile and load the circuit', async function () {
+    expect(circuit).to.not.be.undefined;
+  });
+  it('should generate witness for circuit with Sha256RSA signature', async () => {
+    const { inputs } = prepareTestData();
+    await circuit.calculateWitness(inputs);
+  });
+});
